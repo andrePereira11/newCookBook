8ec2d2a6a0a87d0447cf9f4e6cf0a9dfa3dfef73
Andre <andreluis.alap@gmail.com>
2019-09-28 09:11:55 -0300

    instalação gem factory_bot_rails

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,13 +2,13 @@ class Recipe < ApplicationRecord
   belongs_to :recipe_type
   belongs_to :cuisine
 
-  #validates :title, presence: true
-  #validates :difficulty, presence: true
-  #validates :cook_time, presence: true
-  #validates :ingredients, presence: true
-  #validates :cook_method, presence: true
-  #validates :recipe_type, presence: true
-  #validates :cuisine, presence: true
+  validates :title, presence: true
+  validates :difficulty, presence: true
+  validates :cook_time, presence: true
+  validates :ingredients, presence: true
+  validates :cook_method, presence: true
+  validates :recipe_type, presence: true
+  validates :cuisine, presence: true
 
   def cook_time_min
     "#{cook_time} minutos"
